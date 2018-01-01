977874d3e0c8ebcae228b70d995ad9f9690fa581
Justin <info@justplayer.de>
2018-01-01 02:19:53 +0100

    Init

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Command Commander
+
+You can wait until the hook "CommandCommanderLoaded" is called and then just use this function:
+
+
+
+CommandCommander.Register("COMMAND", {
+		Function = 	function(ply) // The function to run if the command is called
+						ply:Kill()
+					end,
+		Check = 	function(ply) // check if ply can run this 
+						return true
+					end,
+		info = 		"Here you can put your help that you can see if you type [Your Prefix]help"
+})
+
+
+to create an command
---
+++ lua/autorun/cc_main.lua
@@ -0,0 +1,49 @@
+CommandCommander = CommandCommander or {} // Main Tabelle
+
+CommandCommander.CommandChar = "!"
+
+
+CommandCommander.List = {
+
+	["help"] = {
+		Check = function(ply)
+			return ply:IsAdmin()
+		end,
+		Func = function(ply)
+				ply:ChatPrint("<<<<<<<<<<<<<<< COMMANDS >>>>>>>>>>>>>>>")
+			for ID, DATA in pairs(CommandCommander.List) do
+				ply:ChatPrint(ID..": "..DATA.HELP)
+			end
+		end,
+		HELP = "Shows the Help about the commands"
+	}
+
+}
+
+CommandCommander.Register = function (Name, Data)
+								if CommandCommander.List[Name] then Error("Tryed to register existing command!: "..Name) end
+								CommandCommander.List[Name] = {
+									Check = Data.Check or function() return true end,
+									Func = Data.Function or function(ply) ply:ChatPrint("Error: Function not set") end,
+									HELP = Data.info or "None Given"
+								}
+								
+							end
+
+hook.Add( "PlayerSay", "CommandCommander_PlayerSay", function( ply, text, team )
+
+	if ( string.sub( text, 1, string.len( CommandCommander.CommandChar )  ) == CommandCommander.CommandChar ) then	
+		local CMD = string.sub( text,  string.len( CommandCommander.CommandChar ) + 1)
+		local CMD_A = string.Explode( " ", CMD )
+		
+		if CommandCommander.List[CMD_A[1]] then 
+			if CommandCommander.List[CMD_A[1]].Check(ply) then
+				CommandCommander.List[CMD_A[1]].Func(ply)
+				return ""
+			end		
+		end
+		
+	end
+end )
+
+hook.Call( "CommandCommanderLoaded" )
